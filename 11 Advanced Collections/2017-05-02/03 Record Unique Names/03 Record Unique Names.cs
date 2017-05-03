6a7c4d02f9b76b4ab7cf30aea64c82da0aa8889d
Bullsized <ivanstefanovbg@gmail.com>
2017-05-04 01:36:37 +0300

    HashSets

---
+++ 11 Advanced Collections/2017-05-02/03 Record Unique Names/03 Record Unique Names.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stopper = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int cycle = 0; cycle < stopper; cycle++)
+            {
+                string givenName = Console.ReadLine();
+                names.Add(givenName);
+            }
+
+            foreach (var nameToBePrinted in names)
+            {
+                Console.WriteLine(nameToBePrinted);
+            }
+        }
+    }
+}
