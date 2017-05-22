f858abef69a5161364638f9bbce9132a31114b70
Bullsized <ivanstefanovbg@gmail.com>
2017-05-22 13:59:07 +0300

    Lambda

---
+++ 12 Lambda and LINQ/2017-05-22/02 Largest 3 Nums/02 Largest 3 Nums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Largest_3_Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> largestList = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            var firstDigitsList = largestList.OrderByDescending(x => x)
+                .Take(3);
+
+            Console.WriteLine(string.Join(" ", firstDigitsList));
+        }
+    }
+}
