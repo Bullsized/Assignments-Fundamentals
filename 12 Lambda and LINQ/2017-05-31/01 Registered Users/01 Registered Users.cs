eeb6a8777eb951338f7cfb04c0d6df9f4d722160
Bullsized <ivanstefanovbg@gmail.com>
2017-05-31 18:35:20 +0300

    Lambda

---
+++ 12 Lambda and LINQ/2017-05-31/01 Registered Users/01 Registered Users.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Registered_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, DateTime> registeredUsernames = new Dictionary<string, DateTime>();
+
+            string inputLine = Console.ReadLine();
+
+            //  .Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries)
+            //  .ToList();
+            while (inputLine != "end")
+            {
+                string[] inputParams = inputLine
+                    .Split(new char[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string username = inputParams[0];
+                DateTime registryDate = DateTime.Parse(inputParams[1]);
+
+                registeredUsernames.Add(username, registryDate);
+
+                inputLine = Console.ReadLine();
+            }
+
+            Dictionary<string, DateTime> orderedUsernames =
+                registeredUsernames
+                .Reverse()
+                .OrderBy(x => x.Value)
+                .Take(5)
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var kvp in orderedUsernames)
+            {
+                Console.WriteLine(kvp.Key);
+            }
+        }
+    }
+}
