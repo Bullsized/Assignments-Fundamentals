c7205d9b91ff409007a8c208d1baa9555405e728
Bullsized <ivanstefanovbg@gmail.com>
2017-06-03 00:36:53 +0300

    Lambda and LINQ

---
+++ 12 Lambda and LINQ/2017-05-31/03 Flatten Dictionary/03 Flatten Dictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Flatten_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var solution = new Dictionary<string, Dictionary<string, string>>();
+
+            while (input != "end")
+            {
+                List<string> dividedStrings = input.Split(' ').ToList();
+                string seeIfStringExists = dividedStrings[0];
+
+                if (true)
+                {
+
+                }
+
+                string copyFirstString = seeIfStringExists;
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
